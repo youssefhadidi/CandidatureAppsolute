d101b01a6e30e848352a2e1ab799d54411d056b8
yousseftp <yhadidi75@gmail.com>
2022-03-01 00:28:29 +0100

    readme ok, pages/[id] et pages/fav/[id] new tab

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,28 @@
-import { createStore, StoreProvider,persist, useStoreState, useStoreActions} from 'easy-peasy'
+import { createStore, StoreProvider, persist, useStoreState, useStoreActions } from 'easy-peasy'
 import IsHydrated from '../components/Layout/isHydrated';
 import Layout from '../components/Layout/Layout'
 import '../styles/globals.css'
 import model from '/model';
 
 
-const store=createStore(persist(model))
+const store = createStore(persist(model))
 
 function MyApp({ Component, pageProps }) {
 
 
-  return(<><StoreProvider store={store}><IsHydrated><Layout/><div className='content'><Component {...pageProps} /></div></IsHydrated></StoreProvider></>)
+  return (<>
+  <head>
+  <title>Appsolute News</title>
+  </head>
+    <StoreProvider store={store}>
+      <IsHydrated>
+        <Layout />
+        <div className='content'>
+          <Component {...pageProps} />
+        </div>
+      </IsHydrated>
+    </StoreProvider>
+  </>)
 
 }
 
